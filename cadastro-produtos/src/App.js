db162bde38cd333f2cb70035b6678d85cca6b1d0
marcelo.bn <marcelobittencourtx@gmail.com>
2020-05-24 15:40:50 -0300

    Add navbar

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import Navbar from './components/navbar'
+
+
 function App() {
   return (
-      <div className="alert alert-dismissible alert-warning">
-         <button type="button" className="close" data-dismiss="alert">&times;</button>
-         <h4 className="alert-heading">Deu certo!</h4>
-         <p className="mb-0">Hello <a href="#" className="alert-link">Bootswatch</a>.</p>
-      </div>
+      <>
+        <Navbar />
+      </>
   );
 }
 
