2ec759fbe89e2c6193c5915a3743d6f166e9b416
marcelo.bn <marcelobittencourtx@gmail.com>
2020-05-24 20:59:50 -0300

    Add rotas

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Navbar from './components/navbar'
-
+import Rotas from './rotas'
 
 function App() {
   return (
       <>
-        <Navbar />
+        <div className="container"> 
+          <Navbar />
+          <Rotas />
+        </div>
       </>
   );
 }
